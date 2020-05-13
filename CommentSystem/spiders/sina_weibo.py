011a501d702a6e98b7574ae7e5936754c02e2df3
qizai <ldzyjf313@163.com>
2020-05-13 23:24:23 +0800

    add sina spider

--- CommentSystem/spiders/sina_weibo.py
+++ CommentSystem/spiders/sina_weibo.py
@@ -14,7 +14,7 @@ class SinaWeiBoSpider(scrapy.spiders):
     allowed_domains = ["m.weibo.cn"]
 
     def start_requests(self):
-        
+        # https://m.weibo.cn/comments/hotflow?max_id=153274557041735&id=4503524602083108&mid=4503524602083108&max_id_type=0
         pass
     pass
 
