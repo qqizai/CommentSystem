c1aed59f5b26d4d1b0e13fed534accbb57152e5b
e <ldzyjf313@163.com>
2020-05-14 10:14:05 +0800

    modify comment

--- CommentSystem/spiders/sina/weibo_comment.py
+++ CommentSystem/spiders/sina/weibo_comment.py
@@ -12,12 +12,16 @@ class SinaWeiBoSpider(Spider):
     name = "sina_weibo"
 
     allowed_domains = ["m.weibo.cn"]
+    comment_url = "https://m.weibo.cn/comments/hotflow?max_id={max_id}&id={id}&mid={mid}&max_id_type={max_id_type}"
+    total = 0
 
     # https://m.weibo.cn/comments/hotflow?max_id=153274557041735&id=4503524602083108&mid=4503524602083108&max_id_type=0
     def start_requests(self):
+        # todo 需要从某个地方接任务，然后开始爬取(可以是消息队列/数据库)
         pass
 
     def parse_comment(self):
+        # TODO 进行解析全部的评论，有下一页继续调用自己进行解析
         pass
 
     pass
